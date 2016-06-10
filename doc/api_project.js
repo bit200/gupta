2c4507f011af7d8bf6faf2a3fd764e0cadafc99e
Ruslan Romanenko <Romanenko@gmail.com>
2016-06-10 17:39:37 +0300

    update validation claim your agency request

--- doc/api_project.js
+++ doc/api_project.js
@@ -6,7 +6,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-06-10T10:18:06.899Z",
+    "time": "2016-06-10T14:32:44.051Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
