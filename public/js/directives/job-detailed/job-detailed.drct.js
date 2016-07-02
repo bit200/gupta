c068d2510f1cadb0e8e4bcaa2d3cc4c93e0a160d
Nikita Zotsik <nikita.novamedia@gmail.com>
2016-07-02 18:09:22 +0300

    Update

---
+++ public/js/directives/job-detailed/job-detailed.drct.js
@@ -0,0 +1,15 @@
+XYZCtrls.directive('jobDetailed', function () {
+    return {
+        restrict: 'E',
+        scope: true,
+        templateUrl: 'js/directives/job-detailed/job-detailed.html',
+        link: function(scope, el, attrs) {
+            scope.attrs = attrs
+            scope.title = attrs.title
+            scope.btns = attrs.btns
+        },
+        controller: ['$scope', function (scope) {
+            console.log('@@@ Scope Post directive', scope.attrs)
+        }]
+    };
+});
