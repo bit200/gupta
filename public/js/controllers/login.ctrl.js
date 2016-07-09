8fbec393aa095c845e33fffa178d1672ef5651fa
Ruslan Romanenko <roruslanvl@gmail.com>
2016-07-09 12:09:15 +0400

    some code refactor

--- public/js/controllers/login.ctrl.js
+++ public/js/controllers/login.ctrl.js
@@ -1,20 +1,19 @@
 var XYZCtrls = angular.module('XYZCtrls');
-XYZCtrls.controller('loginCtrl', ['$scope', '$location', '$http', '$stateParams', 'AuthService', '$state', function ($scope, location, $http, stateParams, AuthService, $state) {
-    var q = {};
-    $scope.signin = function (invalid, data) {
-        $scope.loginError = '';
-        if (invalid) return;
-        $http.get('/sign-in', {params: {email: data.email, password: data.password}}).success(function (resp) {
-            AuthService.setTokens({
-                accessToken: resp.data.accessToken.value,
-                refreshToken: resp.data.refreshToken.value
-            });
-            $state.go('home')
-        }).error(function (err) {
-            if (err.error == 'Item not found')
-                $scope.loginError = 'User with this login not found';
-            else
-                $scope.loginError = 'Password not correct'
+XYZCtrls.controller('loginCtrl', ['$scope', '$http', 'AuthService', '$state', function ($scope, $http, AuthService, $state) {
+        $scope.signin = function (invalid, data) {
+            $scope.loginError = '';
+            if (invalid) return;
+            $http.get('/sign-in', {params: {email: data.email, password: data.password}}).success(function (resp) {
+                AuthService.setTokens({
+                    accessToken: resp.data.accessToken.value,
+                    refreshToken: resp.data.refreshToken.value
+                });
+                $state.go('home');
+            }).error(function (err) {
+                if (err.error == 'Item not found')
+                    $scope.loginError = 'User with this login not found';
+                else
+                    $scope.loginError = 'Password not correct'
         });
     };
 }])
