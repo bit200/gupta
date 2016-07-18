975b2bd401705c55708659ac357c2e80bff87ac0
Nikita Zotsik <nikita.novamedia@gmail.com>
2016-07-18 11:08:11 +0300

    Update

--- public/js/controllers/viewMyJob.ctrl.js
+++ public/js/controllers/viewMyJob.ctrl.js
@@ -1,16 +1,18 @@
 /* Controllers */
 var XYZCtrls = angular.module('XYZCtrls');
 
-XYZCtrls.controller('ViewMyJobCtrl', ['$scope', '$http', 'info', '$rootScope', '$q', 'getContent', function (scope, $http, info, rootScope, $q, getContent) {
-    console.log('paramssss infofofofoofofofofo', info)
-    var user_type = info.user_type;
-    var job_type = info.job_type;
+XYZCtrls.controller('ViewMyJobCtrl', ['$scope', '$http', 'info', '$rootScope', '$q', 'getContent', function (scope, $http, _info, rootScope, $q, getContent) {
+    alert('ViewMyJobCtrl')
+    var user_type = _info.user_type;
+    var job_type = _info.job_type;
+    var info = angular.copy(_info)
+
     info.template = info.template || [user_type, job_type].join('-');
     info.header = info.header || job_type + ' jobs';
     info.url = info.url || ['/api', 'jobs', user_type, job_type].join('/');
     scope.info = info;
     rootScope.info = info;
-    console.log('info',scope.info)
+    console.log('info', scope.info)
 }]);
 
 XYZCtrls.controller('JobsContentCtrl', ['$scope', 'getContent', '$rootScope', 'jobInformation', function (scope, getContent, $rootScope, jobInformation) {
